2f928bd724fe63e6c83caba26120de0a5bf642d5
Ihor Borunov <borunov65@gmail.com>
2025-03-20 13:45:57 +0200

    Solution

--- app/main.py
+++ app/main.py
@@ -20,8 +20,7 @@ class Animal:
 class Cat(Animal):
 
     def __init__(self, name: str, is_hungry: bool = True) -> None:
-        super().__init__(name, appetite=3)
-        self.is_hungry = is_hungry
+        super().__init__(name, appetite=3, is_hungry=is_hungry)
 
     @staticmethod
     def catch_mouse() -> None:
